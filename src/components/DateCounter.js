43a07c91a88dda11229a0e0de0ee8e561b6cbc90
Daniel White <danielrx-8@hotmail.com>
2026-02-04 18:08:49 -0500

    Agregando funciones y comentarios
    Se agregan comentarios explicando lo aprendido relacionado al uso de useReducer

--- src/components/DateCounter.js
+++ src/components/DateCounter.js
@@ -1,18 +1,45 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
+/* LA FUNCION REDUCER GENERALMENTE RECIBE ESTOS DOS PARAMETROS SIEMPRE "STATE" Y "ACTION"  */
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "dec") return state - 1;
+
+  /* TAMBIEN ES MUY COMUN UTILIZAR SWTICH EN VEZ DE IF PARA VALIDAR LOS DATOS */
+  /* EN ESTE CASO EL SWITCH VALIDA EL "action.type" QUE ES EL DISPATCH ESTABLECIDO EN EL useReducer */
+  /* AL SER EL STATE UN OBJETO QUE CONTIENE COUNT Y STEP, ES NECESARIO RETORNAR UN NUEVO OBJETO CON EL MISMO
+    DISEÑO DEL OBJETO ORIGINAL PERO AGREGANDO LA FUNCION QUE SE DESEA. EN ESTE CASO SUMAR SI ES "INC" 
+    Y RESTAR SI ES "DEC"  */
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+
+    default:
+      throw new Error("Unknow action");
+  }
+
+  /* if (action.type === "dec") return state - 1;
   if (action.type === "inc") return state + 1;
-  if (action.type === "setCount") return action.payload;
+  if (action.type === "setCount") return action.payload; */
 }
 
 function DateCounter() {
   /* const [count, setCount] = useState(0); */
+  /* const [step, setStep] = useState(1); */
 
-  const [count, dispatch] = useReducer(reducer, 0);
+  /* AL UTILIZAR useReducer POR LO GENERAL SE UTILIZA UN OBJETO COMO VALOR INICIAL */
+  const initialState = { count: 0, step: 1 };
 
-  const [step, setStep] = useState(1);
+  /* "sate" HACE REFERENCIA AL ESTADO QUE BASICAMANTE CONTIENE LOS DATOS DE "initialState" */
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  /*  AQUI SE DESTRUCTURAN LOS DATOS QUE ESTAN DENTRO DE "state" EN ESTE CASO "count" y "step" */
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -36,12 +63,13 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
+    /* setStep(Number(e.target.value)); */
   };
 
   const reset = function () {
     /* setCount(0); */
-    setStep(1);
+    /* setStep(1); */
   };
 
   return (
